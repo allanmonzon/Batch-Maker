4d1625125cd4f441d31952cd97e8dce736c68d1a
Allan Monzon <allanjmonzon@gmail.com>
2014-10-14 18:37:03 -0400

    adds index layout

--- app/js/router.js
+++ app/js/router.js
@@ -1,3 +1,12 @@
-Crack.Router.map(function() {
-  this.route('login');
+Batch.Router.map(function() {
+	this.route('login');
+
+  	// this.resource('recipes', function(){
+	  this.route('my-recipes');
+	  this.route('public-recipes');
+	  this.route('popular-recipes');
+	  this.route('my-favorite-recipes');
+	  this.route('my-pantry');
+  	// })
+
 });
